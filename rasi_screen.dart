c6a50e14777f1e04faa5be52aef0ff0f579e5dc5
karthiksmart790-oss <karthiksmart790@gmail.com>
2025-12-30 23:46:09 +0530

    Update rasi_screen.dart

--- rasi_screen.dart
+++ rasi_screen.dart
@@ -1,1 +1,21 @@
+import 'package:flutter/material.dart';
 
+class RasiScreen extends StatelessWidget {
+  const RasiScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ராசி பலன்'),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Text(
+          '12 ராசிகளுக்கான பலன் Loading...',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
