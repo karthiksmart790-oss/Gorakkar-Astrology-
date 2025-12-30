a8f92809d257416f47239cec23fa013179610afd
karthiksmart790-oss <karthiksmart790@gmail.com>
2025-12-30 23:50:47 +0530

    Update parigaram_screen.dart

--- parigaram_screen.dart
+++ parigaram_screen.dart
@@ -1,1 +1,21 @@
+import 'package:flutter/material.dart';
 
+class ParigaramScreen extends StatelessWidget {
+  const ParigaramScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('பரிகாரம்'),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Text(
+          'கோரக்கர் சித்தர் பரிகாரம் Loading...',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
